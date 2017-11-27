5df24a9197ba65978003403c9cd7d5617c8dd45f
Don Tregonning <dtregonning@splunk.com>
2017-11-27 15:23:18 -0800

    default command line values, javadoc work, cleanup

--- src/main/java/generator/CommandLineParams.java
+++ src/main/java/generator/CommandLineParams.java
@@ -3,46 +3,58 @@ package generator;
 import org.kohsuke.args4j.Option;
 
 /**
- * Created by dtregonning on 9/13/17.
+ * CommandLineParams is the class implementation of commandline parameters for the kafka-data-gen application.
+ *
+ * @author      Don Tregonning (dtregonning)
+ * @version     1.0
+ * @since       1.0
  */
 public class CommandLineParams {
-    @Option(name="-message-count", usage="Sets amount of messages to create")
+    @Option(name="-message-count", usage="(Required)Sets amount of messages to create and send to kafka topic")
     public String messageCount;
 
-    @Option(name="-message-size", usage="Sets size of messages to create")
+    @Option(name="-message-size", usage="(Required)Sets size of messages to create")
     public String messageSize;
 
-    @Option(name="-eps", usage="Amount of events per second to send to Kafka")
+    @Option(name="-eps", usage="(Required)Amount of events per second to send to Kafka")
     public String eps;
 
-    @Option(name="-worker-thread-count", usage="Kafka buffer amount")
+    @Option(name="-worker-thread-count", usage="Event generating worker threads, default = 4")
     public String workerThreadCount;
 
-    @Option(name="-topic", usage="Kafka Topic tp send messages to")
+    @Option(name="-topic", usage="(Required)Kafka Topic to send messages to")
     public String topic;
 
-    @Option(name="-bootstrap.servers", usage="Kafka Servers to send messages to")
+    @Option(name="-bootstrap.servers", usage="(Required) Kafka Servers to send messages to")
     public String bootStrapServers;
 
-    @Option(name="-acks", usage="Acknowledgement Scheme (all, 1, 0)")
+    @Option(name="-acks", usage="Acknowledgement Scheme (all, 1, 0),  default = all")
     public String acks;
 
-    @Option(name="-kafka-retries", usage="Kafka retries amount")
+    @Option(name="-kafka-retries", usage="Kafka retries amount,  default = 0")
     public String retries;
 
-    @Option(name="-kafka-batch-size", usage="Kafka batch size amount")
+    @Option(name="-kafka-batch-size", usage="Kafka batch size amount,  default = 1000")
     public String kafkaBatchSize;
 
-    @Option(name="-kafka-linger", usage="Kafka linger setting(ms)")
+    @Option(name="-kafka-linger", usage="Kafka linger setting(ms) ,  default = 1ms")
     public String kafkaLingerms;
 
-    @Option(name="-kafka-buffer-memory", usage="Kafka buffer amount")
+    @Option(name="-kafka-buffer-memory", usage="Kafka buffer amount,  default = 16384")
     public String kafkaBufferMemory;
 
-    public void run() {
-        System.out.println("Command Line Parameters");
-        System.out.println("- message-count: " + messageCount);
-        System.out.println("- message-size: " + messageSize);
-        System.out.println("- topic: " + topic);
+    public String toString() {
+        return "[Command Line Parameters]"
+        + "{ message-count: " + messageCount
+        + ", message-size: " + messageSize
+        + ", topic : " + topic
+        + ", eps: " + eps
+        + ", worker-thread-count: " + workerThreadCount
+        + ", bootstrap.servers: " + bootStrapServers
+        + ", acks: " + acks
+        + ", kafka-retries: " + retries
+        + ", kafka-batch-size: " + kafkaBatchSize
+        + ", kafka-linger: " + kafkaLingerms
+        + ", kafka-buffer-memory: " + kafkaBufferMemory + "}";
     }
 }
--- src/main/java/generator/MetricsCalculator.java
+++ src/main/java/generator/MetricsCalculator.java
@@ -1,4 +1,5 @@
 package generator;
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
@@ -7,35 +8,84 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
+/**
+ * MetricsCalculator is a class responsible for producing consolidated metric counts from multiple threads and
+ * returning a string and metric based on the parameter sent through.
+
+ *
+ * @author      Don Tregonning (dtregonning)
+ * @version     1.0
+ * @since       1.0
+ */
 public class MetricsCalculator {
-    Producer<String, String>[] producerArray;
+    /** Single Log4J Logger for class. */
     private static Logger logger = LogManager.getLogger(EPSThread.class);
 
-    int arrayIndex = 0;
+    /** Array of Kafka Producers to track and return metrics for. */
+    protected static Producer<String, String>[] producerArray;
 
+    /** Current index value for the producerArray */
+    private static int producerArrayIndex = 0;
+
+    /**
+     * Constructor implements the static array producerArray with the correct size based on the configurable
+     * commandline parameter CommandLineParams.workerThreadCount.
+     *
+     * @param workerThreadCount       the amount of worker threads configured within kafka-data-gen
+
+     * @see             Producer
+     * @since           1.0
+     */
     public MetricsCalculator(int workerThreadCount) {
         producerArray = (Producer<String, String>[])new Producer[workerThreadCount];
     }
 
-    public synchronized void addProducer(Producer<String, String> producer) {
-        producerArray[arrayIndex] = producer;
-        logger.info("Im here");
-        arrayIndex++;
+    /**
+     * addProducer will add a Kafka Producer to the class array producerArray. Adding producers to this
+     * array is used as a central lookup of the current producers inside of worker threads.
+     *
+     * @param  producer the amount of worker threads configured within kafka-data-gen
+     * @return          <code>true</code>if the producer is successfully added to array.
+     *                  <code>false</code> otherwise.
+     *
+     * @since           1.0
+     */
+    public synchronized boolean addProducer(Producer<String, String> producer) {
+        try {
+            producerArray[producerArrayIndex] = producer;
+            producerArrayIndex++;
+        } catch (Exception Ex) {
+            logger.error(Ex.toString());
+            return false;
+        }
+          return true;
     }
-
-    public void getMetrics() {
-        double recordSendRate = 0;
-        for(int i = 0; i < arrayIndex; i++){
+    /**
+     * method used for fetching metrics from the following
+     * https://docs.confluent.io/current/kafka/monitoring.html#producer-metrics.
+     *
+     *
+     *
+     * @param   metricName  Name of metric to
+     * @param   metricGroup Group of metric
+     * @return              double value consolidatedMetricValue which will bring together metric value from all
+     *                      worker threads and return a single value.
+     *
+     * @since               1.0
+     */
+    public double getKafkaProducerMetrics(String metricName, String metricGroup) {
+        double consolidatedMetricValue = 0;
+        for(int i = 0; i < producerArrayIndex; i++){
             Producer<String, String> producer = producerArray[i];
             Map<MetricName, ? extends Metric> metrics = producer.metrics();
             for (Map.Entry<MetricName, ? extends Metric> entry : metrics.entrySet()) {
                 String name = entry.getKey().name();
                 String group = entry.getKey().group();
-                if (name.equalsIgnoreCase("record-send-rate") && group.equalsIgnoreCase("producer-metrics")) {
-                    recordSendRate += entry.getValue().value();
+                if (name.equalsIgnoreCase(metricName) && group.equalsIgnoreCase(metricGroup)) {
+                    consolidatedMetricValue += entry.getValue().value();
                 }
             }
         }
-        System.out.println("Current Record Send Rate is: " + recordSendRate);
+        return consolidatedMetricValue;
     }
 }
